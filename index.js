5631f56cb48d7205c6d20786dfb89fe2b429cd3f
jackedCode-r <127081528+jackedCode-r@users.noreply.github.com>
2026-03-14 10:41:50 +0530

    Update server port and greeting message

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const http = require('http');
-const port = process.env.PORT || 3010;
+const port = process.env.PORT || 3020;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello my name is soham and this is my Node project!\n'
   res.end(msg);
 });
 
 server.listen(port, () => {
-  console.log(`Server running on the http://localhost:${port}/!!!!!!`);
+  console.log(`Server running on the http://localhost:${port}/`);
 });
